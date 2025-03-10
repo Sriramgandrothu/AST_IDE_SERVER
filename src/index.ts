dfefb7ccefbd41561e84cf2e280edd600aa54de1
SriramGandrothu <sriramgandrothu@gmail.com>
2025-03-10 23:56:22 +0530

    Localhost

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ app.use(cookieParser());
 app.use(
   cors({
     credentials: true,
-    origin: ["http://localhost:5173", "https://ast-ide.vercel.app/"],
+    origin: "https://ast-ide.vercel.app/",
   })
 );
 config();
