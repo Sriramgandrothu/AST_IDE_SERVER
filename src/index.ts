5d020e3a59108d9b8627ea00758193b065148da7
SriramGandrothu <sriramgandrothu@gmail.com>
2025-03-11 00:08:16 +0530

    Verifytoken in index

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import { dbConnect } from "./lib/dbConnect";
 import { compilerRouter } from "./routes/compilerRouter";
 import { userRouter } from "./routes/userRouter";
 import cookieParser from "cookie-parser";
+import { verifyToken } from "./middlewares/verifyToken";
 const app = express();
 
 app.use(express.json());
@@ -12,13 +13,13 @@ app.use(cookieParser());
 app.use(
   cors({
     credentials: true,
-    origin: "https://ast-ide.vercel.app",
+    origin: ["http://localhost:5173", "https://ast-ide.vercel.app/"],
   })
 );
 config();
 
-app.use("/compiler", compilerRouter);
-app.use("/user", userRouter);
+app.use("/compiler",verifyToken, compilerRouter);
+app.use("/user",verifyToken, userRouter);
 app.get("/health", (req, res) => {
   res.status(200).json({ status: "OK", message: "Server is running smoothly" });
 });
